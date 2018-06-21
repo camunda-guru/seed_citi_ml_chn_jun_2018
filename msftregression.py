f9a183cdfb1610e5c826b6310985431d46d40be4
eswaribala <Parameswaribala@gmail.com>
2018-06-21 11:54:38 +0530

    Add files via upload

---
+++ msftregression.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 13 12:31:33 2018
+
+@author: Balasubramaniam
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('MSFT.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 4:5].values
+print(X,y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/8, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+#Mean Sqaured Error
+MSE=np.mean((y_test-y_pred)**2)
+print("Mean Sqaured Error %r" %(MSE))
+#Sum of Sqaured Error
+#SSE=np.sum((y_test-y_pred)**2)
+#print("SUM of Sqaured Error %r" %(SSE))
+# Visualising the Training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_test, regressor.predict(X_test), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
